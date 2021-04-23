db27e5579a454690dd39ff6ac8a2e625cabed174
drearyArchon <13518099@std.stei.itb.ac.id>
2021-04-23 23:18:37 +0700

    Created NewsAPI

---
+++ app/src/main/java/com/haverzard/workitout/models/News.kt
@@ -0,0 +1,10 @@
+package com.haverzard.workitout.models
+
+data class News(
+    val id: Int? = null,
+    val name: String? = null,
+    val userName: String? = null,
+    val email: String? = null,
+    val phone: String? = null,
+    val website: String? = null
+)
---
+++ app/src/main/java/com/haverzard/workitout/ui/news/NewsAPIClient.kt
@@ -0,0 +1,31 @@
+package com.haverzard.workitout.ui.news
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewsAPIClient {
+    private val BASE_URL: String = ""
+
+    private val gson : Gson by lazy {
+        GsonBuilder().setLenient().create()
+    }
+
+    private val httpClient : OkHttpClient by lazy {
+        OkHttpClient.Builder().build()
+    }
+
+    private val retrofit : Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    val apiService : NewsAPIService by lazy{
+        retrofit.create(NewsAPIService::class.java)
+    }
+}
