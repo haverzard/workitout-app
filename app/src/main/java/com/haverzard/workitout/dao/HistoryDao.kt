d4e8a60618e17500c4ee32ae40a244dceb4c3346
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-04-27 17:42:30 +0700

    add get flow for history

--- app/src/main/java/com/haverzard/workitout/dao/HistoryDao.kt
+++ app/src/main/java/com/haverzard/workitout/dao/HistoryDao.kt
@@ -4,6 +4,7 @@ import androidx.room.*
 import com.haverzard.workitout.entities.History
 import com.haverzard.workitout.entities.RoutineExerciseSchedule
 import kotlinx.coroutines.flow.Flow
+import java.sql.Date
 
 @Dao
 interface HistoryDao  {
@@ -11,8 +12,11 @@ interface HistoryDao  {
     @Query("SELECT * FROM histories")
     fun getHistories(): Flow<List<History>>
 
+    @Query("SELECT * FROM histories WHERE date = :date")
+    fun getHistories(date: String): Flow<List<History>>
+
     @Query("SELECT * FROM histories WHERE id = :id")
-    fun getHistory(id: Int): History
+    fun getHistory(id: Int): Flow<History>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(history: History)
