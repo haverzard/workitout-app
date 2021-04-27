0a498e9ece673f186884528b561a6fe4f500e8f9
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-04-27 17:47:16 +0700

    implement history list page

---
+++ app/src/main/java/com/haverzard/workitout/ui/history/HistoryListAdapter.kt
@@ -0,0 +1,106 @@
+package com.haverzard.workitout.ui.history
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.haverzard.workitout.R
+import com.haverzard.workitout.entities.ExerciseType
+import com.haverzard.workitout.entities.History
+import java.sql.Date
+
+
+class HistoryListAdapter(historySelectedListener: HistorySelectedListener) : ListAdapter<History, HistoryListAdapter.HistoryViewHolder>(
+    HistoriesComparator()
+) {
+
+    private val historySelectedListener: HistorySelectedListener = historySelectedListener
+
+    interface HistorySelectedListener {
+        fun onHistorySelected(history: History)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        return HistoryViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        val history = getItem(position)
+        val date = Date(
+            history.date.year - 1900,
+            history.date.month,
+            history.date.date
+        ).toLocaleString()
+        val time = "%02d:%02d - %02d:%02d".format(
+            history.start_time.hours,
+            history.start_time.minutes,
+            history.end_time.hours,
+            history.end_time.minutes,
+        )
+        var target = ""
+        if (history.exercise_type == ExerciseType.Cycling) {
+            target = "Reached: %.2f %s".format(
+                history.target_reached,
+                "km",
+            )
+        } else {
+            target = "Reached: %d %s".format(
+                history.target_reached.toInt(),
+                "steps",
+            )
+        }
+        holder.itemView.findViewById<ImageButton>(R.id.show_detail).setOnClickListener {
+            historySelectedListener.onHistorySelected(history)
+        }
+        holder.bind(history.exercise_type, date.substring(0, date.length - 9), time, target)
+    }
+
+    class HistoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton: ImageButton = itemView.findViewById(R.id.schedule_exercise)
+        private val dateItemView: TextView = itemView.findViewById(R.id.schedule_date)
+        private val timeItemView: TextView = itemView.findViewById(R.id.schedule_time)
+        private val targetItemView: TextView = itemView.findViewById(R.id.schedule_target)
+
+        fun bind(type: ExerciseType?, date: String?, time: String?, target: String?) {
+            if (type == ExerciseType.Cycling) {
+                imageButton.setImageResource(R.drawable.ic_directions_bike_white_24dp)
+            } else {
+                imageButton.setImageResource(R.drawable.ic_directions_walk_white_24dp)
+            }
+            dateItemView.text = date
+            timeItemView.text = time
+            targetItemView.text = target
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): HistoryViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.history_item, parent, false)
+                return HistoryViewHolder(
+                    view
+                )
+            }
+        }
+    }
+
+    class HistoriesComparator : DiffUtil.ItemCallback<History>() {
+        override fun areItemsTheSame(
+            oldItem: History,
+            newItem: History
+        ): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: History,
+            newItem: History
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+}
--- app/src/main/java/com/haverzard/workitout/ui/history/HistoryListFragment.kt
+++ app/src/main/java/com/haverzard/workitout/ui/history/HistoryListFragment.kt
@@ -1,2 +1,54 @@
 package com.haverzard.workitout.ui.history
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.haverzard.workitout.R
+import com.haverzard.workitout.WorkOutApplication
+import com.haverzard.workitout.entities.History
+
+class HistoryListFragment : Fragment() {
+
+    private lateinit var historyViewModel: HistoryViewModel
+    private val safeArgs: HistoryListFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        historyViewModel = ViewModelProviders.of(
+            this, HistoryViewModelFactory((activity?.application as WorkOutApplication).repository)
+        ).get(HistoryViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_history_list, container, false)
+
+        val textView = root.findViewById<TextView>(R.id.no_history)
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = HistoryListAdapter(object :
+            HistoryListAdapter.HistorySelectedListener {
+            override fun onHistorySelected(history: History) {
+                val action =
+                    HistoryListFragmentDirections.actionHistoryList(history.id)
+                findNavController().navigate(action)
+            }
+        })
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        historyViewModel.getHistories(safeArgs.date)
+        historyViewModel.histories.observe(this) { histories ->
+            textView.visibility = if (histories.isEmpty()) View.VISIBLE else View.GONE
+            histories.let { adapter.submitList(it) }
+        }
+        return root
+    }
+}
