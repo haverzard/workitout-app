ae97c62f188af4d7a157be7ee1dc08db3641ef76
Yonatan <48679462+haverzard@users.noreply.github.com>
2021-04-27 17:47:00 +0700

    implement history home page

--- app/src/main/java/com/haverzard/workitout/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/haverzard/workitout/ui/history/HistoryFragment.kt
@@ -1,58 +1,31 @@
 package com.haverzard.workitout.ui.history
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.observe
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.navigation.fragment.findNavController
 import com.haverzard.workitout.R
-import com.haverzard.workitout.WorkOutApplication
-import com.haverzard.workitout.entities.Day
-import com.haverzard.workitout.entities.RoutineExerciseSchedule
-import com.haverzard.workitout.entities.SingleExerciseSchedule
-import com.haverzard.workitout.receivers.ScheduleReceiver
-import com.haverzard.workitout.ui.schedule.ScheduleListAdapter
-import com.haverzard.workitout.ui.schedule.ScheduleTypeDialog
-import com.haverzard.workitout.viewmodel.ScheduleViewModel
-import com.haverzard.workitout.viewmodel.ScheduleViewModelFactory
 
 
 class HistoryFragment : Fragment() {
-
-    private lateinit var scheduleViewModel: ScheduleViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        scheduleViewModel = ViewModelProviders.of(
-            this, ScheduleViewModelFactory((activity?.application as WorkOutApplication).repository)
-        ).get(ScheduleViewModel::class.java)
-
         val root = inflater.inflate(R.layout.fragment_history, container, false)
 
-//        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
-//        val adapter = ScheduleListAdapter()
-//        recyclerView.adapter = adapter
-//        recyclerView.layoutManager = LinearLayoutManager(activity)
+        val calendarView = root.findViewById<CalendarView>(R.id.calendar)
+        calendarView.setOnDateChangeListener { _, year, month, day ->
+            val action =
+                HistoryFragmentDirections.actionHistory("%04d-%02d-%02d".format(year, month+1, day))
+            findNavController().navigate(action)
+        }
 
-//        scheduleViewModel.schedules.observe(owner = this) { schedules ->
-//            schedules.let { adapter.submitList(it) }
-//        }
         return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 }
