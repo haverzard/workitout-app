03ba2d9d77fa50cfaff25754c4ff32e9ccb61522
mfiras1920 <13518117@std.stei.itb.ac.id>
2021-04-30 16:28:39 +0700

    add magnetic field sensor and accelerometer (still bugged)

--- app/src/main/java/com/haverzard/workitout/ui/tracker/TrackerFragment.kt
+++ app/src/main/java/com/haverzard/workitout/ui/tracker/TrackerFragment.kt
@@ -37,11 +37,15 @@ class TrackerFragment : Fragment(), SensorEventListener {
 
     private lateinit var scheduleViewModel: ScheduleViewModel
     private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private lateinit var magnetometer: Sensor
     private lateinit var image: ImageView
     private lateinit var sharedPreferences: SharedPreferences
 
     private var currentDegree = 0f
     private var exerciseType = ""
+
+    private var azimut = 0f // View to draw a compass
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,8 +59,13 @@ class TrackerFragment : Fragment(), SensorEventListener {
         val root = inflater.inflate(R.layout.fragment_tracker, container, false)
         image = root.findViewById(R.id.compass)
         sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) as Sensor
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) as Sensor
         sharedPreferences =
-            activity!!.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            activity!!.getSharedPreferences(
+                getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE
+            )
 
         if (!permissionApproved()) {
             requestForegroundPermissions()
@@ -75,24 +84,38 @@ class TrackerFragment : Fragment(), SensorEventListener {
             SharedPreferenceUtil.saveExerciseType(activity!!, "")
         } else {
             trackButton.text = getString(R.string.stop_tracking_btn)
-            trackButton.backgroundTintList = ContextCompat.getColorStateList(context!!, android.R.color.holo_red_dark)
+            trackButton.backgroundTintList = ContextCompat.getColorStateList(
+                context!!,
+                android.R.color.holo_red_dark
+            )
             root.findViewById<LinearLayout>(R.id.btn_container).visibility = View.INVISIBLE
             exerciseType = SharedPreferenceUtil.getExerciseType(activity!!)!!
         }
         walkingButton.setOnClickListener {
             walkingButton.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.selected))
-            cyclingButton.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
+            cyclingButton.setBackgroundColor(
+                ContextCompat.getColor(
+                    activity!!,
+                    R.color.colorPrimary
+                )
+            )
             exerciseType = "Walking"
         }
         cyclingButton.setOnClickListener {
             cyclingButton.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.selected))
-            walkingButton.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
+            walkingButton.setBackgroundColor(
+                ContextCompat.getColor(
+                    activity!!,
+                    R.color.colorPrimary
+                )
+            )
             exerciseType = "Cycling"
         }
 
         trackButton.setOnClickListener {
             val enabled = sharedPreferences.getBoolean(
-                SharedPreferenceUtil.KEY_TRACKING_ENABLED, false)
+                SharedPreferenceUtil.KEY_TRACKING_ENABLED, false
+            )
             if (exerciseType == "") {
                 Toast.makeText(
                     activity,
@@ -105,11 +128,24 @@ class TrackerFragment : Fragment(), SensorEventListener {
             val serviceIntent = Intent(activity!!, TrackingService::class.java)
             if (enabled) {
                 activity!!.stopService(serviceIntent)
-                cyclingButton.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
-                walkingButton.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
+                cyclingButton.setBackgroundColor(
+                    ContextCompat.getColor(
+                        activity!!,
+                        R.color.colorPrimary
+                    )
+                )
+                walkingButton.setBackgroundColor(
+                    ContextCompat.getColor(
+                        activity!!,
+                        R.color.colorPrimary
+                    )
+                )
                 root.findViewById<LinearLayout>(R.id.btn_container).visibility = View.VISIBLE
                 (it as MaterialButton).text = getString(R.string.start_tracking_btn)
-                it.backgroundTintList = ContextCompat.getColorStateList(context!!, android.R.color.holo_green_dark)
+                it.backgroundTintList = ContextCompat.getColorStateList(
+                    context!!,
+                    android.R.color.holo_green_dark
+                )
             } else {
                 if (!permissionApproved()) {
                     requestForegroundPermissions()
@@ -132,6 +168,8 @@ class TrackerFragment : Fragment(), SensorEventListener {
             sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
             SensorManager.SENSOR_DELAY_GAME
         )
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI)
     }
 
     override fun onPause() {
@@ -146,7 +184,8 @@ class TrackerFragment : Fragment(), SensorEventListener {
     ) {
         when (requestCode) {
             REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
-                grantResults.isEmpty() -> {}
+                grantResults.isEmpty() -> {
+                }
                 grantResults[0] == PackageManager.PERMISSION_GRANTED ->
                     println("Permission granted!")
                 else -> {
@@ -175,15 +214,49 @@ class TrackerFragment : Fragment(), SensorEventListener {
         }
     }
 
+    var mGravity: FloatArray? = null
+    var mGeomagnetic: FloatArray? = null
+
     override fun onSensorChanged(event: SensorEvent) {
+//        // get angle
+////        println(event.values[0])
+//        val degree = event.values[0].roundToInt().toFloat()
+//
+//        // create rotate animation
+//        val anim = RotateAnimation(
+//            currentDegree,
+//            -degree,
+//            Animation.RELATIVE_TO_SELF,
+//            0.5f,
+//            Animation.RELATIVE_TO_SELF,
+//            0.5f
+//        )
+//        anim.duration = 180
+//        anim.fillAfter = true
+//        image.startAnimation(anim)
+//
+//        currentDegree = -degree
+
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) mGravity = event.values
+        if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) mGeomagnetic = event.values
+        if (mGravity != null && mGeomagnetic != null) {
+            val R = FloatArray(9)
+            val I = FloatArray(9)
+            val success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic)
+            if (success) {
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(R, orientation)
+                azimut = orientation[0] // orientation contains: azimut, pitch and roll
+            }
+        }
+
         // get angle
 //        println(event.values[0])
-        val degree = event.values[0].roundToInt().toFloat()
 
         // create rotate animation
         val anim = RotateAnimation(
             currentDegree,
-            -degree,
+            -azimut,
             Animation.RELATIVE_TO_SELF,
             0.5f,
             Animation.RELATIVE_TO_SELF,
@@ -193,18 +266,18 @@ class TrackerFragment : Fragment(), SensorEventListener {
         anim.fillAfter = true
         image.startAnimation(anim)
 
-        currentDegree = -degree
+        currentDegree = -azimut
     }
 
     private fun permissionApproved(): Boolean {
         return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
-                context!!,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
+            context!!,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
             && PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
-                context!!,
-                Manifest.permission.ACTIVITY_RECOGNITION
-            )
+            context!!,
+            Manifest.permission.ACTIVITY_RECOGNITION
+        )
     }
 
     private fun requestForegroundPermissions() {
@@ -220,7 +293,10 @@ class TrackerFragment : Fragment(), SensorEventListener {
                     // Request permission
                     ActivityCompat.requestPermissions(
                         activity!!,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACTIVITY_RECOGNITION),
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACTIVITY_RECOGNITION
+                        ),
                         REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
                     )
                 }
@@ -228,7 +304,10 @@ class TrackerFragment : Fragment(), SensorEventListener {
         } else {
             ActivityCompat.requestPermissions(
                 activity!!,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACTIVITY_RECOGNITION),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACTIVITY_RECOGNITION
+                ),
                 REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
             )
         }
